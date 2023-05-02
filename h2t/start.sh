6caf3418fbd7b756df31644d15bef373b55f9bf9
Lajos Papp <lalyos@yahoo.com>
2023-05-02 19:36:13 +0200

    fix help

--- h2t/start.sh
+++ h2t/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# -lt 1 ]]; then
-  echo please specify URL. optionally use ENCODING env var to set input (def: UTF8)
+  echo "please specify URL. optionally use ENCODING env var to set input (def: UTF8)"
   exit 1
 fi
 curl -sL $1 | html2text -from_encoding ${ENCODING:-UTF8}  -o -
