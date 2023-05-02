82cd64f9b24d31f2cf111acbc08c51ff2d87f508
Lajos Papp <lalyos@yahoo.com>
2023-05-02 19:07:42 +0200

    use UTF8 encoding by default

--- h2t/start.sh
+++ h2t/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# -lt 1 ]]; then
-  echo please specify URL
+  echo please specify URL. optionally use ENCODING env var to set input (def: UTF8)
   exit 1
 fi
-curl -L $1 | html2text
+curl -sL $1 | html2text -from_encoding ${ENCODING:-UTF8}  -o -
